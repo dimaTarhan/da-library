9bed32761879efac784ae9a4a0ce55a4e50574fd
Anastasiia Yemets <40547968+anastasiia-emets@users.noreply.github.com>
2018-11-28 01:02:44 +0200

    ad

--- src/js/jsFunctions.js
+++ src/js/jsFunctions.js
@@ -1,12 +1,12 @@
 window.onload = function () {
     var linkItems = document.getElementsByClassName('da-sidebar__link');
     var elementsWraper = document.getElementsByClassName('da-sidebar__nav');
-    elementsWraper[0].addEventListener("click", function () {
+    elementsWraper[0].onclick = function () {
         var target = event.target;
         if (!target.classList.contains('da-sidebar__link')) return;
         for (var i=0; i < linkItems.length; i++){
             linkItems[i].classList.remove('da-sidebar__active');
         }
         target.classList.add('da-sidebar__active');
-    });
+    };
 };
