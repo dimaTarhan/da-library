013698f6d0c119d955bcf991a41e501fb3db961e
anastasiia-emets <nastia4019@gmail.com>
2018-12-09 17:19:12 +0200

    delete

--- src/js/slider2.js
+++
@@ -1,8 +0,0 @@
-var slider = document.getElementById("myRange3");
-
-var output = document.getElementById("demo3");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
