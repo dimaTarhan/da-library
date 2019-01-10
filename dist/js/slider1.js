476bf1d524dd6530479c49d108bb0210cc895035
Dmytro Tarhan <43604553+dimaTarhan@users.noreply.github.com>
2019-01-10 22:03:24 +0200

    remove slider1

--- dist/js/slider1.js
+++
@@ -1,8 +0,0 @@
-var slider = document.getElementById("myRange2");
-
-var output = document.getElementById("demo2");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
