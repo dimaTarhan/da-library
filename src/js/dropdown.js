335e41cff4a2d28a66d642f346be5139eaede112
Dmytro Tarhan <43604553+dimaTarhan@users.noreply.github.com>
2019-01-10 22:36:17 +0200

    fix event problem

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
   document.addEventListener("click", event => {
     if (event.target.matches(".da-menu-button")) {
       event.target.nextElementSibling.classList.toggle("visible");
@@ -10,8 +10,5 @@ window.onload = function() {
         currentDropdown.querySelector(".da-menu").classList.remove("visible");
       }
     });
-    if (event.target.closest(".da-menu")) {
-      console.dir(event.target.innerText);
-    }
   });
-};
+});
